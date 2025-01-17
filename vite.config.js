c78d9b9322e5d1e8040705000595361806be6ec9
moonknight1729 <prathamruhela@gmail.com>
2025-01-18 02:48:42 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://json-server-re5z.vercel.app',
+        target: 'https://alurageek-api-ecru.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
