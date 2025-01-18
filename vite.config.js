00a9c88908eb2042c64c7eb91ec684d831208bbd
moonknight1729 <prathamruhela@gmail.com>
2025-01-18 10:17:31 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    strictPort: true,
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'https://alurageek-api-ecru.vercel.app/',
+      "/api": {
+        target: "https://alurageek-api-ecru.vercel.app/",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
